1881306006c4db4db99e94cdfe4559bf8e828118
chand1012 <chandlerl2000@gmail.com>
2020-08-13 13:06:01 -0400

    sorted functions better

---
+++ boosted.go
@@ -0,0 +1,93 @@
+package main
+
+import "database/sql"
+
+func getAllBoostedUsers() ([]boostedUser, error) {
+	db, err := initDB()
+
+	defer db.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	tempMap := make(map[string]boostedUser)
+
+	rows, err := db.Query("SELECT userID, status, guildID FROM boosted")
+
+	var userID string
+	var status uint8
+	var guildID string
+
+	for rows.Next() {
+		err = rows.Scan(&userID, &status, &guildID)
+		if err != nil {
+			return nil, err
+		}
+
+		user, ok := tempMap[userID]
+
+		if ok {
+			user.Guilds = append(user.Guilds, guildID)
+			tempMap[userID] = user
+		} else {
+			user = boostedUser{
+				ID:     userID,
+				Status: status,
+				Guilds: []string{guildID},
+			}
+			tempMap[userID] = user
+		}
+	}
+
+	var users []boostedUser
+
+	for _, v := range tempMap {
+		users = append(users, v)
+	}
+
+	return users, nil
+
+}
+
+// func setBoostedUser(userID string, status uint8, guild string) error {
+// 	db, err := initDB()
+
+// 	defer db.Close()
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	insert, err := db.Prepare("INSERT INTO boosted (userID, status, guilds, cooldown) VALUES ?, ?, ?, ?")
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return err
+// 	}
+
+// 	cooldown := time.Now().Unix() + 2700000
+
+// 	_, err = insert.Exec(userID, status, guild, cooldown)
+// 	insert.Close()
+
+// 	return err
+// }
+
+func removeBoostedUser(userID string) error {
+	db, err := initDB()
+
+	defer db.Close()
+
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec("DELETE FROM boosted WHERE userID = ?", userID)
+
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+
+	return nil
+}
---
+++ patron.go
@@ -0,0 +1,65 @@
+package main
+
+import "database/sql"
+
+func addPatronToDB(userID string, status uint8) error {
+	db, err := initDB()
+
+	defer db.Close()
+
+	if err != nil {
+		return err
+	}
+
+	insert, err := db.Prepare("INSERT INTO patrons (userID, status) VALUES ?, ?")
+
+	defer insert.Close()
+
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(userID, status)
+
+	return err
+}
+
+func removePatronFromDB(userID string) error {
+	db, err := initDB()
+
+	defer db.Close()
+
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec("DELETE FROM patrons WHERE userID = ?", userID)
+
+	return err
+}
+
+// from patron.go on the main bot
+func getPatronStatus(userID string) (uint8, error) {
+	db, err := initDB()
+
+	defer db.Close()
+
+	if err != nil {
+		return 0, err
+	}
+
+	output, err := db.Prepare("SELECT status FROM patron WHERE userID = ?")
+
+	defer output.Close()
+
+	var status uint8
+
+	err = output.QueryRow(userID).Scan(&status)
+
+	if err == sql.ErrNoRows {
+		return 0, nil
+	} else if err != nil {
+		return 0, err
+	}
+
+	return status, nil
+}
